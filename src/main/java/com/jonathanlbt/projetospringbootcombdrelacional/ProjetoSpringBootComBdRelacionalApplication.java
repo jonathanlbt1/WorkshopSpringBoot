f5ebb6fa71e369abd921d42a223dcd932bd6ad15
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2022-11-13 18:50:13 -0300

    Primeiro Commit

---
+++ src/main/java/com/jonathanlbt/projetospringbootcombdrelacional/ProjetoSpringBootComBdRelacionalApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanlbt.projetospringbootcombdrelacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootComBdRelacionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpringBootComBdRelacionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonathanlbt/projetospringbootcombdrelacional/ProjetoSpringBootComBdRelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathanlbt.projetospringbootcombdrelacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootComBdRelacionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
