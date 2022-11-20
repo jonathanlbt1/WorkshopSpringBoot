d91294215e08ee9d87170248bf7234d7a196de33
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2022-11-20 13:46:03 -0300

    Exception handling

--- src/main/java/com/jonathanlbt/projetospringbootcombdrelacional/services/UserService.java
+++ src/main/java/com/jonathanlbt/projetospringbootcombdrelacional/services/UserService.java
@@ -2,6 +2,7 @@ package com.jonathanlbt.projetospringbootcombdrelacional.services;
 
 import com.jonathanlbt.projetospringbootcombdrelacional.entities.User;
 import com.jonathanlbt.projetospringbootcombdrelacional.repositories.UserRepository;
+import com.jonathanlbt.projetospringbootcombdrelacional.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
